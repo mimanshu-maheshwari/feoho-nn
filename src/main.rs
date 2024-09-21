8c5e5652d7548cf639b926c9dcbee1f9674fe475
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2024-09-22 01:59:12 +0530

    Add basic matrix multiplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use feoho_nn::{Matrix, Result};
+
+fn main() -> Result<()> {
+    let test_data = vec![1.0, 2.0, 2.0, 4.0, 3.0, 6.0, 4.0, 8.0];
+    let test_data = Matrix::from(4, 2, &test_data);
+    let identity = Matrix::identity(2, 4);
+    let zero = Matrix::zero(3, 3);
+    println!("Test data: {}", test_data);
+    println!("Identity: {}", identity);
+    println!("Zero: {}", zero);
+    println!("test * identity: {}", &test_data * &identity);
+    println!("Identity * test: {}", &identity * &test_data);
+
+    Ok(())
 }
--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::Mul};
 
 use crate::NNET;
 
@@ -9,14 +9,21 @@ pub struct Matrix {
     data: Vec<NNET>,
 }
 
+#[allow(unused)]
 impl Matrix {
 
-    fn _zero(rows: usize, cols: usize) -> Self {
+    pub fn from(rows: usize,cols: usize, data: &[NNET]) -> Self {
+        assert_eq!(data.len(), rows * cols, "ERROR:Size of data is not equal to given rows and cols.");
+        let data = data.to_vec();
+        Self {rows, cols, data}
+    }
+
+    pub fn zero(rows: usize, cols: usize) -> Self {
         let data = vec![0.0; rows * cols];
         Self { rows, cols, data}
     }
 
-    fn _identity(rows: usize, cols: usize) -> Self {
+    pub fn identity(rows: usize, cols: usize) -> Self {
         let mut data = vec![0.0; rows * cols];
         let min = usize::min(rows,cols);
         for i in 0..min {
@@ -25,38 +32,65 @@ impl Matrix {
         Self { rows, cols, data}
     }
 
-    fn _get_row_count(&self) -> usize  {
+    fn get_row_count(&self) -> usize  {
         self.rows
     }
 
-    fn _get_col_count(&self) -> usize  {
+    fn get_col_count(&self) -> usize  {
         self.cols
     }
 
-    fn _get_ref(&self, row: usize, col: usize) -> &NNET {
-        assert!(row < self.rows, "ERROR: Given row {} is greater than number of rows available, i.e. {}", row, self.rows);
-        assert!(col < self.cols, "ERROR: Given col {} is greater than number of cols available, i.e. {}", col, self.cols);
+    fn get_ref(&self, row: usize, col: usize) -> &NNET {
+        assert!(row < self.rows, "ERROR: Given row {} is greater than or equal to number of rows available, i.e. {}", row, self.rows);
+        assert!(col < self.cols, "ERROR: Given col {} is greater than or equal to number of cols available, i.e. {}", col, self.cols);
         &self.data[row * self.cols + col]
     }
 
-    fn _get_ref_mut(&mut self, row: usize, col: usize) -> &mut NNET {
-        assert!(row < self.rows, "ERROR: Given row {} is greater than number of rows available, i.e. {}", row, self.rows);
-        assert!(col < self.cols, "ERROR: Given col {} is greater than number of cols available, i.e. {}", col, self.cols);
+    fn get_ref_mut(&mut self, row: usize, col: usize) -> &mut NNET {
+        assert!(row < self.rows, "ERROR: Given row {} is greater than or equal to number of rows available, i.e. {}", row, self.rows);
+        assert!(col < self.cols, "ERROR: Given col {} is greater than or equal to number of cols available, i.e. {}", col, self.cols);
         &mut self.data[row * self.cols + col]
     }
 
-    fn _get_row_ref(&self, row: usize) -> &[NNET] {
-        assert!(row < self.rows, "ERROR: Given row {} is greater than number of rows available, i.e. {}", row, self.rows);
+    fn get_row_ref(&self, row: usize) -> &[NNET] {
+        assert!(row < self.rows, "ERROR: Given row {} is greater than or equal to number of rows available, i.e. {}", row, self.rows);
         let start = row * self.cols;
         &self.data[start..(start + self.cols)]
     }
 
-    fn _get_row_ref_mut(&mut self, row: usize) -> &mut [NNET] {
-        assert!(row < self.rows, "ERROR: Given row {} is greater than number of rows available, i.e. {}", row, self.rows);
+    fn get_row_ref_mut(&mut self, row: usize) -> &mut [NNET] {
+        assert!(row < self.rows, "ERROR: Given row {} is greater than or equal to number of rows available, i.e. {}", row, self.rows);
         let start = row * self.cols;
         &mut self.data[start..(start + self.cols)]
     }
 
+    pub fn get_data_ref(&self) -> &[NNET] {
+        &self.data
+    }
+
+    pub fn get_data_ref_mut(&mut self) -> &mut [NNET] {
+        &mut self.data
+    }
+}
+
+impl Mul<&Matrix> for &Matrix {
+    type Output = Matrix;
+    fn mul(self, rhs: &Matrix) -> Self::Output {
+        assert_eq!(self.cols, rhs.rows, "ERROR: Can't multiply matrix as the inner size is not equal.");
+        let rows = self.rows;
+        let cols = rhs.cols;
+        let mid = self.cols;
+        let mut result = Matrix::zero(rows, cols);
+        for row in 0..rows {
+            for col in 0..cols {
+                *result.get_ref_mut(row, col) = 0.0;
+                for k in 0..mid {
+                    *result.get_ref_mut(row, col) += self.get_ref(row, k) * rhs.get_ref(k, col);
+                }
+            }
+        }
+        result
+    }
 }
 
 impl Display for Matrix {
